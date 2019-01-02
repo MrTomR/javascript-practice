146608f62226d25c11e2abc9b3b79aed4fd40001
Tom Robbins <tomrobbins@toms-mac-mini.kembrey.local>
2019-01-02 17:43:20 +0000

    Working on carousel

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,34 @@
 $(document).ready(function(){
-    console.log("hello world"); 
+    $(".next").on('click', function() {
+        var currentImg = $('.active');
+        var nextImg = currentImg.next();
+
+        if($(".slider-inner img").last().hasClass('active')) {
+            nextImg = $(".slider-inner img").first();
+        }
+
+        if(nextImg.length) {
+            currentImg.removeClass('active').css('z-index', -10);
+            nextImg.addClass('active').css('z-index', 10);
+        }
+    });
+
+    $(".prev").on('click', function() {
+        var currentImg = $('.active');
+        var prevImg = currentImg.prev();
+
+        if($(".slider-inner img").first().hasClass('active')) {
+            prevImg = $(".slider-inner img").last();
+        }
+
+        if(prevImg.length) {
+            currentImg.removeClass('active').css('z-index', -10);
+            prevImg.addClass('active').css('z-index', 10);
+        }
+
+        console.log(prevImg.length);
+        console.log(currentImg.length);
+    });
+
 
 });
--- page-one.php
+++ page-one.php
@@ -2,7 +2,19 @@
     include('includes/header.php');
 ?>
 
-This is page 1
+    <div class="container">
+        <h1>JQ Slider</h1>
+        <div class="slider-outer">
+            <img src="assets/images/sliderimages/arrow-left.png" alt="Prev" class="prev">
+            <div class="slider-inner">
+                <img src="assets/images/sliderimages/image1.jpg" class="active" alt="">
+                <img src="assets/images/sliderimages/image2.jpg" alt="">
+                <img src="assets/images/sliderimages/image3.jpg" alt="">
+                <img src="assets/images/sliderimages/image4.jpg" alt="">
+            </div>
+            <img src="assets/images/sliderimages/arrow-right.png" alt="Next" class="next">
+        </div>
+    </div>
 
 
 <?php
